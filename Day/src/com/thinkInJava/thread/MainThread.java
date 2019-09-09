39a7aa717d70489310c6933b30f8d70c3ebc2c9d
gaoxupeng <252211829@qq.com>
2019-09-09 23:52:07 +0800

    刷绿

--- Day/src/com/thinkInJava/thread/MainThread.java
+++ Day/src/com/thinkInJava/thread/MainThread.java
@@ -2,9 +2,22 @@ package com.thinkInJava.thread;
 
 import com.thinkInJava.thread.LiftoFF;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainThread {
-    public static void main(String[] args){
-        LiftoFF liftoFF = new LiftoFF();
-        liftoFF.run();
+    public static void main(String[] args) {
+//        LiftoFF liftoFF = new LiftoFF();
+//        liftoFF.run();
+
+//        for (int i=0;i<5;i++){
+//            new Thread(new LiftoFF()).start();
+
+        ExecutorService executors = Executors.newCachedThreadPool();
+
+        for (int i=0;i<5;i++) {
+            executors.execute(new LiftoFF());
+            executors.shutdown();
+        }
     }
 }
