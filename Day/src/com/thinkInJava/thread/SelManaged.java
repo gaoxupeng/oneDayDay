8dbd2c561020c27139de64821103f7428f720f3a
gaoxupeng <252211829@qq.com>
2019-09-11 23:19:50 +0800

    1

---
+++ Day/src/com/thinkInJava/thread/SelManaged.java
@@ -0,0 +1,28 @@
+package com.thinkInJava.thread;
+
+public class SelManaged implements Runnable{
+    private int countDown = 5;
+    private Thread t = new Thread(this);
+    public SelManaged(){t.start();}
+
+    @Override
+    public String toString() {
+        return Thread.currentThread().getName()+"("+ countDown +")";
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            System.out.println(this);
+            if (--countDown ==0){
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i =0;i<5;i++){
+            new SelManaged();
+        }
+    }
+}
